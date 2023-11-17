aa4aea02186f45e2de284a78ed41f292b36cdeb3
tyler kim <yoonsoo.kim11@gmail.com>
2023-11-17 13:56:43 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+return fetch('https://anapioficeandfire.com/api/books')
+.then(response => response.json())
+.then(json => renderBooks(json))
 }
+
+
+ 
 
 function renderBooks(books) {
   const main = document.querySelector('main');
+  console.log(books)
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
